c685f5a34a10036f996b80573a612c710a3c7a43
goat000000000 <pracowityziomek67@proton.me>
2026-06-02 13:58:40 +0200

    Dodanie i zbudowanie strony by ja udostepnic i modyfikacja by wygladala nieco lepiej na smartfonach

--- prodzekt/src/data/local_data.js
+++ prodzekt/src/data/local_data.js
@@ -1,7 +1,7 @@
 export const LISTA_DATA = [
   {
     id: 1,
-    nazwa: 'Zabrać tabletki',
+    nazwa: 'Zabrać leki',
     zaznaczone: 1
   },
   {
@@ -9,9 +9,29 @@ export const LISTA_DATA = [
     nazwa: 'Zjeść śniadanie',
     zaznaczone: 0
   },
+    {
+    id: 3,
+    nazwa: 'Umyć się',
+    zaznaczone: 0
+  },
+  {
+    id: 4,
+    nazwa: 'Pojechać do szkoły/pracy',
+    zaznaczone: 0
+  },
+  {
+    id: 5,
+    nazwa: 'Przekąsić coś',
+    zaznaczone: 0
+  },
+  {
+    id: 6,
+    nazwa: 'Zjeść obiad',
+    zaznaczone: 0
+  },
   {
     id: 3,
-    nazwa: 'Wyjść ze szkoły',
-    zaznaczone: 1
+    nazwa: 'Iść spać przed 23:00',
+    zaznaczone: 0
   }
   ];
